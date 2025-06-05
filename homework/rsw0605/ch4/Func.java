beab809b351594d3071f3d47c7e1417f7ac4e2c4
ryumax99 <ryumax99@gmail.com>
2025-06-05 16:13:39 +0900

    0605 7교시

--- homework/rsw0605/ch4/Func.java
+++ homework/rsw0605/ch4/Func.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 import util.date.DateUtil;
 
 public class Func {
-    static final int MAX_CARS = 100; 
+    static final int MAX_CARS = 100;
 
     static Model[] models = new Model[MAX_CARS];
 
-    static int carCount = 0; 
+    static int carCount = 0;
 
     public static void addCar(Scanner scanner) {
-        if (carCount < MAX_CARS) { 
+        if (carCount < MAX_CARS) {
             System.out.println("자동차명을 입력하세요: ");
             String carname = scanner.nextLine();
 
@@ -27,20 +27,20 @@ public class Func {
             Model model = new Model(carname, manufacturer, vin, regDate);
 
             models[carCount] = model;
-           
+
             carCount++;
-            System.out.println("자동차가 추가되었습니다 :  차종 : " + carname + ", 제조사 :  " + manufacturer +  ", 연식 : " + vin + ", 등록일 : " + regDate);
+            System.out.println("자동차가 추가되었습니다 :  차종 : " + carname + ", 제조사 :  " + manufacturer + ", 연식 : " + vin
+                    + ", 등록일 : " + regDate);
             System.out.println("===============================================================");
         } else {
             System.out.println("자동차 수가 최대치를 초과했습니다.");
         }
-    } 
-
+    }
 
     public static void viewCars() {
         if (carCount == 0) {
             System.out.println("등록된 자동차가 없습니다.");
-            return; 
+            return;
         }
         System.out.println("등록된 자동차 목록:");
         System.out.println("===============================================================");
@@ -48,73 +48,66 @@ public class Func {
             models[i].showInfo();
             System.out.println("===============================================================");
         }
-    } 
+    }
 
     public static void updateCar(Scanner scanner) {
         System.out.println("수정할 자동차의 인덱스를 입력하세요 (0 ~ " + (carCount - 1) + "): ");
-        int index = scanner.nextInt(); 
-        scanner.nextLine(); 
+        int index = scanner.nextInt();
+        scanner.nextLine();
         if (index < 0 || index >= carCount) {
             System.out.println("잘못된 인덱스입니다.");
             return;
         }
         System.out.println("수정할 자동차의 이름을 입력하세요: ");
         String carname = scanner.nextLine();
-       
 
         System.out.println("수정할 자동차의 제조사를 입력하세요: ");
         String manufacturer = scanner.nextLine();
-       
 
         System.out.println("수정할 자동차의 연식을 입력하세요: ");
         String vin = scanner.nextLine();
-     
-
 
         String regDate = DateUtil.getCurrentDateTime();
-   
-
 
         Model model = new Model(carname, manufacturer, vin, regDate);
 
         models[index] = model;
 
         System.out.println("자동차 정보가 수정되었습니다: " + carname + ", " + manufacturer + ", "
-                + ", " + vin + ", 수정일자 :  " + regDate );
-    } 
+                + ", " + vin + ", 수정일자 :  " + regDate);
+    }
 
     public static void deleteCar(Scanner scanner) {
         System.out.println("삭제할 자동차의 인덱스를 입력하세요 (0 ~ " + (carCount - 1) + "): ");
-        int index = scanner.nextInt(); 
-        scanner.nextLine(); 
+        int index = scanner.nextInt();
+        scanner.nextLine();
 
         if (index < 0 || index >= carCount) {
             System.out.println("잘못된 인덱스입니다.");
-            return; 
+            return;
         }
 
         Model model = models[index];
         System.out.println("삭제할 자동차 정보: ");
-        model.showInfo(); 
+        model.showInfo();
 
         for (int i = index; i < carCount - 1; i++) {
-            models[i] = models[i + 1]; 
+            models[i] = models[i + 1];
         }
-        
-        models[carCount - 1] = null; 
 
-     
+        models[carCount - 1] = null;
+
         carCount--;
         System.out.println("자동차 정보가 삭제되었습니다.");
     }
-    
+
     public static void addDummyCars() {
         for (int i = 0; i < 5; i++) {
             if (carCount < MAX_CARS) {
                 Model dummyModel = new Model(
                         "더미자동차" + (i + 1),
                         "제조사" + (i + 1),
-                        "연식" + (i + 1) ,
+                        "연식" + (i + 1),
                         DateUtil.getCurrentDateTime());
                 models[carCount] = dummyModel;
                 carCount++;
@@ -140,10 +133,11 @@ public class Func {
             }
         }
 
-        if (!found) { 
+        if (!found) {
             System.out.println("검색 결과가 없습니다.");
         }
     }
+
     public static void car_main(Scanner scanner) {
         int menu;
         do {
@@ -154,7 +148,7 @@ public class Func {
             System.out.println("===============================================================");
             System.out.print("메뉴를 선택하세요(0 ~ 6): ");
             menu = scanner.nextInt();
-            scanner.nextLine(); 
+            scanner.nextLine();
             switch (menu) {
                 case 1:
                     Func.addCar(scanner);
